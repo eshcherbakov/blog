49fdf32fb897075604200c77e303d653e2951044
Евгений Щербаков < >
2017-03-04 08:19:21 +0500

    Security

---
+++ src/main/java/org/parma/domain/User.java
@@ -0,0 +1,49 @@
+package org.parma.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.tomcat.util.bcel.classfile.Constant;
+import org.hibernate.validator.constraints.Email;
+import org.parma.config.Constants;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "sequenceGenerator", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Pattern(regexp = Constants.LOGIN_REGEX)
+    @Size(min = 1, max = 50)
+    @Column(length = 50, unique = true, nullable = false)
+    private String login;
+
+    @NotNull
+    @Size(min = 60, max = 60)
+    @Column(name = "password", length = 60)
+    private String password;
+
+    @Size(max = 50)
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+
+    @Size(max = 50)
+    @Column(name = "last_name", length = 50)
+    private String lastName;
+
+    @Email
+    @Size(max = 100)
+    @Column(length = 100, unique = true)
+    private String email;
+}
